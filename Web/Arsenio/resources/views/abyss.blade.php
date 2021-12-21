b6d611e2db9d1a8bcd4f7fba0e022ec412e256b3
MASTERLIM27 <andreaslim2002@gmail.com>
2021-12-21 15:10:00 +0700

    Question Seeder Complete

--- Web/Arsenio/resources/views/abyss.blade.php
+++ Web/Arsenio/resources/views/abyss.blade.php
@@ -8,11 +8,11 @@
     <div class="float-end">
         
         <div class="abyss-parent">
-            <img class="mt-3 img-fluid abyss-leaderboard" src="/images/leaderboard.png" alt="Leaderboard" width="280">
+            <img class="mt-3 img-fluid abyss-leaderboard" src="/images/leaderboard.png" alt="Leaderboard" width="350">
             <div class="abyss-text">
             @foreach ($studentPoint as $sp)
                 @if ($loop->index < 5)
-                    <h5>{{ $loop->index+1 }}. {{ $sp->user->name}} ({{ $sp['abyss_point']}} pt)</h5>   
+                    <h4>{{ $loop->index+1 }}. {{ $sp->user->name}} ({{ $sp['abyss_point']}} pt)</h4>   
                 @else
                     @break             
                 @endif          
@@ -20,11 +20,11 @@
             </div>
         </div>
 
-        <a href="/battle/n/abyss/n/n/{{ $student->characterExp->health }}/t/0/n/25/n"><img class="m-4 img-fluid abyss-btn" src="/images/buttonAbyss.png" alt="button" width="160"/></a>
+        <a href="/battle/n/abyss/n/n/{{ $student->characterExp->health }}/t/0/n/25/n"><img class="m-4 img-fluid abyss-btn" src="/images/buttonAbyss.png" alt="button" width="200"/></a>
     
     </div>
     <div>
-        <img class="m-3 img-fluid abyss-monster" src="/images/monsterAbyss.png" alt="Monster" width="650">
+        <img class="m-5 img-fluid abyss-monster" src="/images/monsterAbyss.png" alt="Monster" width="850">
     </div>
 </body>
 
